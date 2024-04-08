29189cf95cd0b098cb2f044d43879b61d4809a84
meheraz1100 <meheraz1100@gmail.com>
2024-04-08 17:38:58 +0600

    nav link component added

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -1,13 +1,17 @@
+import PropTypes from 'prop-types';
+
 const Link = ({route}) => {
     return (
         <div>
-            <li className="mr-10">
+            <li className="mr-10 px-6 hover:bg-yellow-500">
                 <a href={route.path}>{route.name}</a>
             </li>
         </div>
     );
 };
 
-Link.proptypes
+Link.propTypes = {
+    route: PropTypes.object
+}
 
 export default Link;
