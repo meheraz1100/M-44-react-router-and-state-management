3afc4f2d1b7a23b91e47ac066a3a960dd81ade57
meheraz1100 <meheraz1100@gmail.com>
2024-04-08 17:39:16 +0600

    navbar added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import Link from "../Link/Link";
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 
 
 const Navbar = () => {
-
+    const [open, setOpen] = useState(false);
     const routes = [
         { id: 1, path: '/', name: 'Home' },
         { id: 2, path: '/about', name: 'About' },
@@ -14,8 +16,18 @@ const Navbar = () => {
       
 
     return (
-        <nav>
-            <ul className="md:flex">
+        <nav className="text-black bg-yellow-200 p-6">
+            <div className={`md:hidden duration-1000 text-2xl`} onClick={() => setOpen(!open)}>
+                {
+                    open === true ? 
+                    <AiOutlineClose></AiOutlineClose> 
+                    : <AiOutlineMenu></AiOutlineMenu>
+                }
+                
+            </div>
+            <ul className={`md:flex duration-1000 absolute md:static
+            ${open ? 'top-16' : '-top-60'}
+            bg-yellow-200 px-6`}>
                 {
                     routes.map(route => <Link key={route.id} route={route}></Link>)
                 }
